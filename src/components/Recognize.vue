97fe96cb3befbc3a3de410fabc312966b49fc482
berkaykarlik <brkykrlk@gmail.com>
2022-08-14 18:29:26 +0300

    instead of rand unicode choose randomly from table

--- src/components/Recognize.vue
+++ src/components/Recognize.vue
@@ -10,16 +10,36 @@
 <script setup>
     import { ref } from 'vue';
 
-    const unicode_hiragana_start = 0x3041;
-    const unicode_hiragana_end = 0x3096;
+    // from https://github.com/WaniKani/WanaKana/blob/master/src/utils/kanaToRomajiMap.js
+    const BASIC_ROMAJI = {
+        あ:'a',    い:'i',   う:'u',   え:'e',    お:'o',
+        か:'ka',   き:'ki',  く:'ku',  け:'ke',   こ:'ko',
+        さ:'sa',   し:'shi', す:'su',  せ:'se',   そ:'so',
+        た:'ta',   ち:'chi', つ:'tsu', て:'te',   と:'to',
+        な:'na',   に:'ni',  ぬ:'nu',  ね:'ne',   の:'no',
+        は:'ha',   ひ:'hi',  ふ:'fu',  へ:'he',   ほ:'ho',
+        ま:'ma',   み:'mi',  む:'mu',  め:'me',   も:'mo',
+        ら:'ra',   り:'ri',  る:'ru',  れ:'re',   ろ:'ro',
+        や:'ya',   ゆ:'yu',  よ:'yo',
+        わ:'wa',   ゐ:'wi',  ゑ:'we',  を:'wo',
+        ん: 'n',
+        が:'ga',   ぎ:'gi',  ぐ:'gu',  げ:'ge',   ご:'go',
+        ざ:'za',   じ:'ji',  ず:'zu',  ぜ:'ze',   ぞ:'zo',
+        だ:'da',   ぢ:'ji',  づ:'zu',  で:'de',   ど:'do',
+        ば:'ba',   び:'bi',  ぶ:'bu',  べ:'be',   ぼ:'bo',
+        ぱ:'pa',   ぴ:'pi',  ぷ:'pu',  ぺ:'pe',   ぽ:'po',
+        // ゔぁ:'va', ゔぃ:'vi', ゔ:'vu',  ゔぇ:'ve', ゔぉ:'vo',
+        };
 
     let letter = ref(null);
 
-    function update_letter(){
-        let rand_hiragana = Math.floor(Math.random() * (unicode_hiragana_end - unicode_hiragana_start + 1)) + unicode_hiragana_start ;
-        letter = ref(String.fromCharCode(rand_hiragana));
-    }
-    update_letter();
+    function random_letter () {
+        var keys = Object.keys(BASIC_ROMAJI);
+        return keys[keys.length * Math.random() << 0];
+    };
+
+    letter.value = random_letter();
+
 </script>
 
 
