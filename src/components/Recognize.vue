3b2baf7c8b98cfb955aee93d0ad087fa9eb5ee4f
berkaykarlik <brkykrlk@gmail.com>
2022-08-14 17:45:16 +0300

    ADD: simple component, remove hello worlds

---
+++ src/components/Recognize.vue
@@ -0,0 +1,28 @@
+
+<template>
+<div>
+    <p id="letter"> {{'\u3041'}}</p>
+    <input type="text" id="answer" name="answer_textbox" placeholder="enter romaji here...">
+</div>
+</template>
+
+
+<script setup>
+
+</script>
+
+
+<style>
+div {
+    display: flexbox;
+    flex-direction: column;
+    text-align: center;
+}
+
+#letter {
+    font-size: 100px;
+    font-family: "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
+    margin-bottom: 10px;
+}
+
+</style>
