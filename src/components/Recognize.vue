e11eca8261db18e7e99f49d35eceb9c0a770c74d
berkaykarlik <brkykrlk@gmail.com>
2022-08-14 18:16:51 +0300

    ADD: generate rand hirahana on component load

--- src/components/Recognize.vue
+++ src/components/Recognize.vue
@@ -1,14 +1,25 @@
 
 <template>
 <div>
-    <p id="letter"> {{'\u3041'}}</p>
+    <p id="letter_p"> {{letter}}</p>
     <input type="text" id="answer" name="answer_textbox" placeholder="enter romaji here...">
 </div>
 </template>
 
 
 <script setup>
+    import { ref } from 'vue';
 
+    const unicode_hiragana_start = 0x3041;
+    const unicode_hiragana_end = 0x3096;
+
+    let letter = ref(null);
+
+    function update_letter(){
+        let rand_hiragana = Math.floor(Math.random() * (unicode_hiragana_end - unicode_hiragana_start + 1)) + unicode_hiragana_start ;
+        letter = ref(String.fromCharCode(rand_hiragana));
+    }
+    update_letter();
 </script>
 
 
@@ -19,7 +30,7 @@ div {
     text-align: center;
 }
 
-#letter {
+#letter_p {
     font-size: 100px;
     font-family: "Hiragino Kaku Gothic ProN", Meiryo, sans-serif;
     margin-bottom: 10px;
