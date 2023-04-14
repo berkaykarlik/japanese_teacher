979dac26a0222b717a67f7193bd6072168cbaa7d
berkaykarlik <brkykrlk@gmail.com>
2023-04-14 23:09:22 +0300

    deploy to pages

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # japanese_teacher
 
-This template should help get you started developing with Vue 3 in Vite.
+Japanese alphabet (hiragana/katakana) practice web application.
 
 ## Recommended IDE Setup
 
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/japanese_teacher/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
